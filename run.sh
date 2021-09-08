d3cde3abd5c36d301bf10acc12b8f48b99008a00
lomot <lomot@qq.com>
2021-09-08 01:15:06 +0000

    quick commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd build
+cmake ..
+make
+./main 3005
---
+++ src/util.cpp
@@ -0,0 +1,12 @@
+
+class Socket {
+private:
+  /* data */
+public:
+  Socket(/* args */);
+  ~Socket();
+};
+
+Socket::Socket(/* args */) {}
+
+Socket::~Socket() {}
